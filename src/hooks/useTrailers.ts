cfbda143273a1fd3ae248a85de0d7eaa4bbf4a2b
Sargun <sargunbhatti27@gmail.com>
2024-01-07 15:12:02 -0500

    built game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/apiClient";
+import { Trailer } from "../services/trailerServices";
+
+
+
+const useTrailers = (gameID: number) =>  { 
+    const apiClient = new APIClient<Trailer>(`/games/${gameID}/movies`);
+
+    return useQuery({
+    queryKey: ['trailers', gameID],
+    queryFn: apiClient.getAll,
+})
+}
+
+export default useTrailers;
