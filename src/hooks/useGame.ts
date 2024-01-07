0e74732a51fdb9429951bb9b781ff1ebfee9b0b3
Sargun <sargunbhatti27@gmail.com>
2024-01-07 13:24:25 -0500

    apparently had updates

--- src/hooks/useGame.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import gameServices from '../services/gameServices'
-
-
-const useGame = (slug: string) => useQuery({
-    queryKey: ['game', slug],
-    queryFn: () => gameServices.get(slug),
-})
-export default useGame
